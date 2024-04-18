da8e4535e2950d7cfc25c27107cbf7530248475c
Jussara Granja <jussaragranja19@gmail.com>
2024-04-18 12:30:57 -0300

    add allure

--- cypress.config.js
+++ cypress.config.js
@@ -6,9 +6,7 @@ module.exports = defineConfig({
     //supportFile: false,
     baseUrl: "https://gorest.co.in/public/v2",
     setupNodeEvents(on, config) {
-      allureCypress(on, {
-        resultsDir: "./allure-cypress/reporter"
-      });
+      allureCypress(on);
     },
   },
 });
